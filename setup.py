664d2360236e55fe8335c1503f514334d47904ae
anselmrothe <anselmrothe@users.noreply.github.com>
2019-03-27 16:30:37 -0400

    rename

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+long_description = open("README.md").read()
 
 setuptools.setup(
-    name="progressbar",
-    version="1.0.4",
+    name="progressbarABC",
+    version="1.0.5",
     author="Anselm Rothe",
     author_email="anselm@nyu.edu",
     description="Progressbar for Python",
     long_description=long_description,
-    # long_description_content_type="text/markdown",
+    long_description_content_type="text/markdown",
     url="https://github.com/anselmrothe/progressbar",
-    # packages=setuptools.find_packages(),
-    packages=['pb',],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPL-3.0 ",
